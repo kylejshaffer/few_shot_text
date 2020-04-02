d7cadb97fd3c34ac6e38c0abe691a6dff15f44d4
Kyle Shaffer <kyle.shaffer@qntfy.com>
2020-04-02 11:30:16 -0400

    updating training code and adding baseline model

--- src/models.py
+++ src/models.py
@@ -11,52 +11,45 @@ class SiameseNet(nn.Module):
         self.hidden_dim = args.hidden_dim
         self.hidden_layers = args.hidden_layers
         self.embed = nn.Embedding(self.vocab_size, self.embed_dim)
-        self.embed_dropout = nn.Dropout(p=0.2)
-        self.dense_dropout = nn.Dropout(p=0.2)
+        self.embed_dropout = nn.Dropout(p=0.25)
+        self.dense_dropout = nn.Dropout(p=0.25)
         self.encoders = nn.ModuleList([nn.Conv1d(in_channels=self.embed_dim, out_channels=self.hidden_dim, kernel_size=3, stride=1),
                                        nn.Conv1d(in_channels=self.hidden_dim, out_channels=self.hidden_dim, kernel_size=3, stride=1),
                                        nn.Conv1d(in_channels=self.hidden_dim, out_channels=self.hidden_dim, kernel_size=3, stride=1)])
+
         # self.dense_hidden = nn.Linear(in_features=int(self.hidden_dim * self.hidden_layers), out_features=512)
         self.pool = nn.AvgPool1d(2)
-        # self.dense_hidden = nn.Linear(in_features=int(2 * (self.hidden_dim * len(self.encoders))), out_features=512)
-        self.dense_hidden = nn.Linear(in_features=int(2 * self.hidden_dim), out_features=512)
+        self.dense_hidden = nn.Linear(in_features=self.hidden_dim, out_features=512)
         self.dense_out = nn.Linear(in_features=512, out_features=1)
 
     def _encode_and_pool(self, x, encoder):
-        # return torch.relu(encoder(x).permute(0, 2, 1).max(1)[0])
         return torch.relu(self.pool(encoder(x)))
 
+    def _encoder(self, x):
+        for e_ix, e in enumerate(self.encoders):
+            x = self._encode_and_pool(x=x, encoder=e)
+
+        return x
+
     def forward(self, x_left, x_right):
-        x_left_embed = self.embed_dropout(self.embed(x_left)).permute(0, 2, 1)
-        x_right_embed = self.embed_dropout(self.embed(x_right)).permute(0, 2, 1)
+        x_left_embed = self.embed_dropout(self.embed(x_left)).permute(0, 2, 1) # -- only permute for conv
+        x_right_embed = self.embed_dropout(self.embed(x_right)).permute(0, 2, 1) # -- only permute for conv
 
-        # x_left = [self._encode_and_pool(x_left, c) for c in self.encoders]
-        # x_right = [self._encode_and_pool(x_right, c) for c in self.encoders]
-        # x_left = torch.cat(x_left, 1)
-        # x_right = torch.cat(x_right, 1)
-
-        # Stacked left encoder output
-        for e_ix, e in enumerate(self.encoders):
-            if e_ix == 0:
-                x_left = self._encode_and_pool(x=x_left_embed, encoder=e)
-            else:
-                x_left = self._encode_and_pool(x=x_left, encoder=e)
-
-        # Stacked right encoder output
-        for e_ix, e in enumerate(self.encoders):
-            if e_ix == 0:
-                x_right = self._encode_and_pool(x=x_right_embed, encoder=e)
-            else:
-                x_right = self._encode_and_pool(x=x_right, encoder=e)
+        # Run encoder
+        x_left = self._encoder(x_left_embed)
+        x_right = self._encoder(x_right_embed)
 
         # Pooling
         x_right = x_right.max(2)[0]
         x_left = x_left.max(2)[0]
+        # print('x_right pooled:', x_right.shape)
+        # print('x_left pooled:', x_left.shape)
 
         # final_encoded = x_left - x_right
         minus = x_left - x_right
-        plus = x_left + x_right
-        final_encoded = torch.cat([torch.abs(minus), plus], 1)
+        # plus = x_left + x_right
+        # final_encoded = torch.cat([torch.abs(minus), plus], 1)
+        final_encoded = torch.abs(minus)
 
         hidden_out = torch.relu(self.dense_hidden(final_encoded))
         logits_out = self.dense_out(self.dense_dropout(hidden_out))
@@ -64,6 +57,32 @@ class SiameseNet(nn.Module):
         return logits_out
 
 
+class ConvClassifier(SiameseNet):
+    def __init__(self, args, vocab_size:int):
+        super().__init__(args, vocab_size)
+        self.dense_hidden = nn.Linear(in_features=int(2 * self.hidden_dim), out_features=512)
+        self.dense_out = nn.Linear(in_features=512, out_features=14)
+
+    def forward(self, x_input):
+        x_embed = self.embed(x_input).permute(0, 2, 1)
+        x_embed = self.embed_dropout(x_embed)
+
+        # Stacked left encoder output
+        x_out = self._encoder(x_embed)
+
+        # Pooling
+        x_out_max = x_out.max(2)[0]
+        x_out_min = x_out.min(2)[0]
+
+        final_encoded = torch.cat([x_out_max, x_out_min], 1)
+
+        hidden_out = torch.relu(self.dense_hidden(final_encoded))
+        logits_out = self.dense_out(self.dense_dropout(hidden_out))
+
+        return logits_out
+
+
+# Run it
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 import numpy as np
 import torch
 import torch.nn as nn
@@ -13,9 +14,6 @@ from data_utils import *
 from one_shot_model import *
 
 
-device = torch.device('cuda')
-
-
 class Trainer:
     def __init__(self, args, train_data, test_data, vocab_size):
         self.model_path = '/data/users/kyle.shaffer/wm/oneshot_models'
@@ -25,10 +23,11 @@ class Trainer:
         self.batch_size = args.batch_size
         self.n_epochs = args.n_epochs
         self.vocab_size = vocab_size
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_loader = None
         self.test_loader = None
         self._setup_data()
-        self.model = SiameseNet(args, vocab_size=self.vocab_size).to(device)
+        self.model = SiameseNet(args, vocab_size=self.vocab_size).to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
 
     def _setup_data(self):
@@ -56,14 +55,14 @@ class Trainer:
             # print('*', flush=True, end='')
             sys.stdout.write('\rEvaluating {} examples...'.format((ix + 1) * x_left.shape[0]))
             with torch.no_grad():
-                x_left, x_right, y = x_left.to(device), x_right.to(device), y.to(device)
+                x_left, x_right, y = x_left.to(self.device), x_right.to(self.device), y.to(self.device)
 
                 output = self.model(x_left, x_right).squeeze()
                 # print('output:', output.shape)
                 losses = self.criterion(output, y)
 
                 total_loss += losses.item() # .data[0]
-                probs = F.sigmoid(output).data.cpu().numpy().tolist()
+                probs = torch.sigmoid(output).data.cpu().numpy().tolist()
                 # print(type(probs))
                 preds = [1 if p >= 0.5 else 0 for p in probs]
                 y_pred.extend(preds)
@@ -84,7 +83,7 @@ class Trainer:
             self.model.train()
             for x_left, x_right, y_batch in self.train_loader:
                 print('=', flush=True, end='')
-                x_left, x_right, y_batch = x_left.to(device), x_right.to(device), y_batch.to(device)
+                x_left, x_right, y_batch = x_left.to(self.device), x_right.to(self.device), y_batch.to(self.device)
                 self.optimizer.zero_grad()
                 outputs = self.model(x_left, x_right).squeeze()
                 loss = self.criterion(outputs, y_batch)
@@ -103,7 +102,87 @@ class Trainer:
             if test_acc > best_acc:
                 print('Saving model...')
                 best_acc = test_acc
-                torch.save(self.model.state_dict(), os.path.join(self.model_path, 'stacked_one_shot_model.pt'))
+                torch.save(self.model.state_dict(), os.path.join(self.model_path, 'conv_one_shot_model.pt'))
+
+
+class ClfTrainer(Trainer):
+    def __init__(self, args, train_data, test_data, vocab_size):
+        super().__init__(args, train_data, test_data, vocab_size)
+        self.criterion = nn.CrossEntropyLoss()
+        self.model = ConvClassifier(args=args, vocab_size=vocab_size).to(self.device)
+
+    def _setup_data(self):
+        train_set = ClfLoader(self.train_data)
+        test_set = ClfLoader(self.test_data)
+        print('Train label distribution:')
+        train_set.get_label_freq()
+        print('Test label distribution:')
+        test_set.get_label_freq()
+        train_sampler = RandomSampler(train_set)
+        test_sampler = RandomSampler(test_set)
+        self.train_loader = DataLoader(train_set, sampler=train_sampler,
+                                       batch_size=self.batch_size, collate_fn=clf_collate_fn)
+        self.test_loader = DataLoader(test_set, sampler=test_sampler,
+                                       batch_size=self.batch_size, collate_fn=clf_collate_fn)
+
+    def evaluate(self, data):
+        self.model.eval()
+
+        data_len = len(data)
+        total_loss = 0.0
+        y_true, y_pred = [], []
+
+        for ix, (x, y) in enumerate(data):
+            # print('*', flush=True, end='')
+            sys.stdout.write('\rEvaluating {} examples...'.format((ix + 1) * x.shape[0]))
+            with torch.no_grad():
+                x, y = x.to(self.device), y.to(self.device)
+
+                output = self.model(x).squeeze()
+                # print('output:', output.shape)
+                losses = self.criterion(output, y)
+
+                total_loss += losses.item() # .data[0]
+                # print(type(probs))
+                preds = torch.argmax(output, dim=1).cpu().tolist()
+                y_pred.extend(preds)
+                # print(y.data.cpu().numpy().tolist()[:10])
+                y_true.extend([int(i) for i in y.data.cpu().numpy().tolist()])
+
+        update_loss = total_loss / data_len
+        print('Test loss:', update_loss)
+        print(metrics.classification_report(np.asarray(y_true), np.asarray(y_pred)))
+        print()
+
+        return metrics.accuracy_score(np.asarray(y_true), np.asarray(y_pred))
+
+    def train(self):
+        best_acc = 0.0
+        for epoch in range(args.n_epochs):
+            total_loss = 0.0
+            self.model.train()
+            for x_batch, y_batch in self.train_loader:
+                print('=', flush=True, end='')
+                x_batch, y_batch = x_batch.to(self.device), y_batch.to(self.device)
+                self.optimizer.zero_grad()
+                outputs = self.model(x_batch).squeeze()
+                loss = self.criterion(outputs, y_batch)
+                total_loss += loss.item()
+                loss.backward()
+                self.optimizer.step()
+
+            print('\nSummary for Epoch {}'.format(epoch + 1))
+            print('Train Loss:', total_loss / len(self.train_loader))
+            print('Train Metrics')
+            train_acc = self.evaluate(self.train_loader)
+            print('Test Metrics')
+            test_acc = self.evaluate(self.test_loader)
+            print()
+
+            if test_acc > best_acc:
+                print('Saving model...')
+                best_acc = test_acc
+                torch.save(self.model.state_dict(), os.path.join(self.model_path, 'db_clf.pt'))
 
 
 def main(args):
@@ -111,7 +190,12 @@ def main(args):
     train_data, test_data = data_formatter.get_data()
     print(len(train_data))
     print(len(test_data))
-    trainer = Trainer(args, train_data, test_data, data_formatter.vocab_size)
+    if args.task == 'oneshot':
+        print('Training one-shot model...')
+        trainer = Trainer(args, train_data, test_data, data_formatter.vocab_size)
+    else:
+        print('Training classifier...')
+        trainer = ClfTrainer(args, train_data, test_data, data_formatter.vocab_size)
     trainer.train()
 
 
@@ -121,6 +205,7 @@ if __name__ == '__main__':
     parser.add_argument('--gpu', type=int, required=False, default=0)
     parser.add_argument('--batch_size', type=int, required=False, default=32)
     parser.add_argument('--n_epochs', type=int, required=False, default=10)
+    parser.add_argument('--task', type=str, required=False, default='oneshot')
     # Model params
     parser.add_argument('--embed_dim', type=int, required=False, default=200)
     parser.add_argument('--hidden_dim', type=int, required=False, default=256)
